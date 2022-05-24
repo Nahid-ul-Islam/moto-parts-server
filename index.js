18f567213d6d9366738faf988348c3b1a427e6be
Nahid-ul-Islam <nahidul522@gmail.com>
2022-05-24 22:08:08 +0600

    load single item api done

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ async function run() {
             res.send(parts);
         });
 
+        //load a single part
+        app.get('/parts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const singlePart = await partsCollection.findOne(query);
+            res.send(query);
+        });
+
 
         //load reviews
         app.get('/reviews', async(req, res) => {
